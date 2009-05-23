c7ab9f46a27071bbea60e5a614040c6779aa4d4e
craig <craig>
2009-05-23 05:32:34 +0000

    Moved all constants associated with the buildErrorMessage() method, which was formerly part of the Unit class and is now part of the Messages class of the new utilities package, to Messages. Moved the following constants from the Unit class to this interface: 	FIRST_COLUMN_LABEL_WIDTH 	SECOND_COLUMN_VALUE_WIDTH 	THIRD_COLUMN_LABEL_WIDTH 	FOURTH_COLUMN_VALUE_WIDTH Eliminated the YES and NO constants.

--- source/jasl/counters/Counter.java
+++ source/jasl/counters/Counter.java
@@ -10,119 +10,51 @@
 //                                                                            //
 // Written By: Craig R. Campbell  -  September 2001                           //
 //                                                                            //
-// $Id: Counter.java,v 1.11 2008/09/10 04:20:54 craig Exp $
+// $Id: Counter.java,v 1.12 2009/05/23 05:32:34 craig Exp $
 // ************************************************************************** //
 
 package jasl.counters;
-
-import java.io.*;
 
 /**
  * This interface is used to define public constants for the classes in the
  * counters package.
  *
- * @version 1.11
+ * @version 1.12
  * @author Craig R. Campbell
  * @see <A HREF="../../../source/jasl/counters/Counter.html">Source code</A>
  */
 
-public interface Counter extends Serializable
+public interface Counter
 {
 	// Public symbolic constants
 
-	// These constants are available to all programs and can be used to specify
-	// parameters for new objects or to build menus.
-
-	// The following strings are used to build the error messages (see below)
-	// that appear when an exception is thrown. Each subclass will have a
-	// CLASS_NAME constant defined in it for this purpose as well.
-
-	// The name of the method throwing the exception. These constants are
-	// defined as necessary within each subclass.
-
 	/**
-	 * Indicates that a message originated in the constructor of a specific
-	 * class : <B>constructor</B>
-	 *
-	 * @see Unit#buildErrorMessage
+	 * The width of the first column of output from the toString() method in
+	 * classes that implement this interface : <B>20</B>.
 	 */
 
-	public static final String CONSTRUCTOR = "constructor";
-
-	// This is a separator used in messages associated with illegal argument
-	// exceptions where there is a conflict between two distinct values (see
-	// checking of unitType in the <A HREF="Fighting.html">Fighting</A> constructor for an example).
+	public static final int FIRST_COLUMN_LABEL_WIDTH = 20;
 
 	/**
-	 * Separates multiple invalid parameters in error messages for exceptions :
-	 * <B> and </B>
-	 *
-	 * @see Unit#buildErrorMessage
+	 * The width of the second column of output from the toString() method in
+	 * classes that implement this interface : <B>17</B>.
 	 */
 
-	public static final String AND_SEPARATOR = " and ";
-
-	// Error messages.
+	public static final int SECOND_COLUMN_VALUE_WIDTH = 17;
 
 	/**
-	 * Indicates that a null parameter was received by a constructor or method :
-	 * <B>Null parameter received.</B>
-	 *
-	 * @see Unit#buildErrorMessage
+	 * The width of the third column of output from the toString() method in
+	 * classes that implement this interface : <B>26</B>.
 	 */
 
-	public static final String NULL_PARAMETER_MSG = "Null parameter received.";
+	public static final int THIRD_COLUMN_LABEL_WIDTH = 26;
 
 	/**
-	 * Indicates that a zero length parameter was received by a constructor or
-	 * method :
-	 * <B>Invalid parameter received (zero length).</B>
-	 *
-	 * @see Unit#buildErrorMessage
+	 * The width of the fourth column of output from the toString() method in
+	 * classes that implement this interface : <B>17</B>.
 	 */
 
-	public static final String ZERO_LENGTH_PARAMETER_MSG =
-		"Invalid parameter received (zero length).";
-
-	/**
-	 * Indicates that one or more invalid parameters was received by a
-	 * constructor or method :
-	 * <B>Invalid parameter(s) received : </B>
-	 *
-	 * @see Unit#buildErrorMessage
-	 */
-
-	public static final String INVALID_PARAMETER_MSG =
-		"Invalid parameter(s) received : ";
-
-	/**
-	 * A label used in the toString() method to build exception messages :
-	 * <B>(toString) - </B>
-	 * It is appended to the class name in each toString() method definition.
-	 *
-	 * @see Unit#buildErrorMessage
-	 */
-
-	public static final String TO_STRING_LABEL = "(toString) - ";
-
-	// The following strings are provided primarily for use in the toString()
-	// method.
-
-	/**
-	 * Indicates the current value of a boolean data member : <B>Yes</B>
-	 *
-	 * @see #toString
-	 */
-
-	public static final String YES = "Yes";
-
-	/**
-	 * Indicates the current value of a boolean data member : <B>No</B>
-	 *
-	 * @see #toString
-	 */
-
-	public static final String NO = "No";
+	public static final int FOURTH_COLUMN_VALUE_WIDTH = 17;
 
 	// Access methods
 
