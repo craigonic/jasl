4d0184af4575b8a1eeba6e3b8f315fc512ebf4ab
craig <craig>
2009-11-28 03:47:21 +0000

    Corrected minor formatting issues. Removed the unused METHOD_LABEL constant from the toString() method, which allowed the CLASS_NAME member constant to be eliminated as well.

--- source/jasl/utilities/Dice.java
+++ source/jasl/utilities/Dice.java
@@ -12,7 +12,7 @@
 //                                                                            //
 // Written By: Craig R. Campbell  -  September 1999                           //
 //                                                                            //
-// $Id: Dice.java,v 1.12 2009/03/28 03:36:18 craig Exp $
+// $Id: Dice.java,v 1.13 2009/11/28 03:47:21 craig Exp $
 // ************************************************************************** //
 
 package jasl.utilities;
@@ -21,7 +21,7 @@ package jasl.utilities;
  * This is a utility class used to provide the simulated result(s) of rolling
  * one or two six sided dice.
  *
- * @version 1.12
+ * @version 1.13
  * @author Craig R. Campbell
  * @see <A HREF="../../../source/jasl/utilities/Dice.html">Source code</A>
  */
@@ -31,16 +31,11 @@ public final class Dice
 	// Private symbolic constants
 
 	// These constants are used to define the minimum and maximum possible
-	// result values for a single die. The MAX_ROLL constant is also used in the
-	// rollIt() method to set the maximum random value.
+	// result values for a single die. The MAX_ROLL constant is also used in
+	// the rollIt() method to set the maximum random value.
 
 	private static final int MIN_ROLL = 1;
 	private static final int MAX_ROLL = 6;
-
-	// This constant is used as part of the error messages (see below) that
-	// that are generated when an exception is thrown.
-
-	private static final String CLASS_NAME = "Dice";
 
 	// Private data members
 
@@ -56,8 +51,8 @@ public final class Dice
 
 	private int combinedResult;
 
-	// This variable is used to store the result of "rolling" a die while it is
-	// checked to verify that it falls within the expected range.
+	// This variable is used to store the result of "rolling" a die while it
+	// is checked to verify that it falls within the expected range.
 
 	private int tmpResult;
 
@@ -65,17 +60,17 @@ public final class Dice
 	// result during the simulated roll of the dice.
 
 	private static final String badResultError =
-		Messages.buildErrorMessage(CLASS_NAME,Messages.CONSTRUCTOR,
+		Messages.buildErrorMessage("Dice",Messages.CONSTRUCTOR,
 		                           "Invalid result : ");
 
 	// Constructor
 
 	/**
-	 * Construct a new <CODE>Dice</CODE> instance. When the object is created, both dice are
-	 * "rolled" automatically.
+	 * Construct a new <CODE>Dice</CODE> instance. When the object is created, both dice
+	 * are "rolled" automatically.
 	 *
-	 * @throws IllegalStateException in the case of an invalid result on one or
-	 * both of the dice.
+	 * @throws IllegalStateException in the case of an invalid result on one
+	 * or both of the dice.
 	 */
 
 	public Dice()
@@ -88,8 +83,9 @@ public final class Dice
 		tmpResult       = 0;
 
 		// Roll the dice. If the method fails, assume that a failure has
-		// occurred in calculating one of the die rolls (the result did not fall
-		// within the expected range). If this happens, throw the exception.
+		// occurred in calculating one of the die rolls (the result did
+		// not fall within the expected range). If this happens, throw
+		// the exception.
 
 		if (!(rollEm()))
 		{
@@ -99,10 +95,10 @@ public final class Dice
 
 	// Private methods
 
-	// rollEm - A method to roll each die, verify that a valid result occurs,
-	//		  and copy the value to the appropriate member variable. If an
-	//		  invalid result occurs, the method returns false, which causes
-	//		  the constructor to throw an exception.
+	// rollEm - A method to roll each die, verify that a valid result
+	//          occurs, and copy the value to the appropriate member
+	//          variable. If an invalid result occurs, the method returns
+	//          false, which causes the constructor to throw an exception.
 
 	private boolean rollEm()
 	{
@@ -110,8 +106,8 @@ public final class Dice
 
 		tmpResult = rollIt();
 
-		// Check the result and return false if it does not fall within the
-		// expected range.
+		// Check the result and return false if it does not fall within
+		// the expected range.
 
 		if ((tmpResult < MIN_ROLL) || (tmpResult > MAX_ROLL))
 		{
@@ -130,8 +126,8 @@ public final class Dice
 
 		tmpResult = rollIt();
 
-		// Check the result and return false if it does not fall within the
-		// expected range.
+		// Check the result and return false if it does not fall within
+		// the expected range.
 
 		if ((tmpResult < MIN_ROLL) || (tmpResult > MAX_ROLL))
 		{
@@ -142,13 +138,13 @@ public final class Dice
 
 		coloredDieValue = tmpResult;
 
-		// Sum the white and colored dice rolls and copy the value to the
-		// combinedResult member variable.
+		// Sum the white and colored dice rolls and copy the value to
+		// the combinedResult member variable.
 
 		combinedResult = whiteDieValue + coloredDieValue;
 
-		// Return true to indicate that all of the die rolls were completed
-		// successfully.
+		// Return true to indicate that all of the die rolls were
+		// completed successfully.
 
 		return true;
 	}
@@ -163,8 +159,8 @@ public final class Dice
 	// Public access methods
 
 	/**
-	 * Display the value of each of the private data members that describe the
-	 * current instance. Each value is preceded by a label.
+	 * Display the value of each of the private data members that describe
+	 * the current instance. Each value is preceded by a label.
 	 *
 	 * @return a <CODE>String</CODE>.
 	 */
@@ -173,8 +169,6 @@ public final class Dice
 	{
 		// Define local constants.
 
-		String METHOD_LABEL = CLASS_NAME + Messages.TO_STRING_LABEL;
-
 		String WHITE_DIE_LABEL       = "White Die";
 		String COLORED_DIE_LABEL     = "Colored Die";
 		String COMBINED_RESULT_LABEL = "Combined Result";
@@ -182,8 +176,8 @@ public final class Dice
 		int    LABEL_WIDTH           = 20;
 		int    VALUE_WIDTH           =  5;
 
-		// Create a buffer to store the string to be returned, initializing it
-		// with the values that define the header.
+		// Create a buffer to store the string to be returned,
+		// initializing it with the values that define the header.
 
 		StringBuffer returnString = new StringBuffer();
 
@@ -244,7 +238,8 @@ public final class Dice
 	/**
 	 * Return the result of combining the values of the two dice.
 	 *
-	 * @return an <CODE>int</CODE> specifying the sum of the white and colored die values.
+	 * @return an <CODE>int</CODE> specifying the sum of the white and colored die
+	 * values.
 	 */
 
 	public int getCombinedResult()
