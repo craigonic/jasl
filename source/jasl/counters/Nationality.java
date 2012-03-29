22d8c49c451fd20cbe90c05c73aeb7153d175263
Craig Campbell <craigonic@gmail.com>
2012-03-29 11:18:43 -0700

    Updated/modernized, replacing the old Vector with an enum.
    Other than documentation updates, the only other real change was renaming
    the getNationality() method to nationality().

--- source/jasl/counters/Nationality.java
+++ source/jasl/counters/Nationality.java
@@ -6,152 +6,145 @@
 //                                                                            //
 //                    NOTE: This program is based on Advanced Squad Leader,   //
 //                          which was created by The Avalon Hill Game         //
-//                          Company, and lives on at <A HREF="http://www.multimanpublishing.com/ASL/asl.php">MultimanPublishing.com</A>.  //
+//                          Company, and lives on at <A HREF="http://www.multimanpublishing.com/Products/tabid/58/CategoryID/4/Default.aspx">Multi-Man Publishing</A>.    //
 //                                                                            //
 // Written By: Craig R. Campbell  -  December 2006                            //
-//                                                                            //
-// $Id: Nationality.java,v 1.3 2007/08/11 05:15:12 craig Exp $
 // ************************************************************************** //
 
 package jasl.counters;
 
-import java.util.*; // For Vector.
-
 /**
- * This interface is used to define public constants and required methods
- * associated with the nationality of <A HREF="Fighting.html">Fighting</A> units.
+ * This interface is used to define the public constants, using an enum, and
+ * required method associated with the nationality of a <A HREF="Fighting.html">Fighting</A> unit. The
+ * method is intended for operation on a String member variable within the
+ * implementing class.
  *
- * @version 1.3
- * @author Craig R. Campbell
+ * @version 2.0
+ * @author Copyright (C) 2006-2012 Craig R. Campbell (craigonic@gmail.com)
  * @see <A HREF="../../../source/jasl/counters/Nationality.html">Source code</A>
  */
 
 public interface Nationality
 {
-    // Recognized nationality values. The nationality variable (see
-    // <A HREF="Fighting.html">Fighting</A>.java) best describes the army that the <A HREF="Unit.html">Unit</A> belongs to, not
-    // necessarily the true nationality of the soldiers in that <A HREF="Unit.html">Unit</A>.
+	// Symbolic constants
 
-    /** <A NAME="_ALLIED_MINOR_"></A>
-     * Indicates that a unit's nationality is <B>Allied Minor</B>.
-     */
+	// This constant is provided primarily for use in displaying the
+	// nationality of a <A HREF="Unit.html">Unit</A> using an objects toString() method.
 
-    public static final String ALLIED_MINOR         = "Allied Minor";
+	/**
+	 * Provides a label for a units nationality : <B>Nationality</B>
+	 */
 
-    /** <A NAME="_AMERICAN_"></A>
-     * Indicates that a unit's nationality is <B>American</B>.
-     */
+	public static final String NATIONALITY_LABEL = "Nationality";
 
-    public static final String AMERICAN             = "American";
+	/**
+	 * Recognized nationality values. The nationality best describes the
+	 * army that the <A HREF="Unit.html">unit</A> belongs to, not necessarily the true nationality
+	 * of the soldiers in it.
+	 */
 
-    /** <A NAME="_AXIS_MINOR_"></A>
-     * Indicates that a unit's nationality is <B>Axis Minor</B>.
-     */
+	public enum Nationalities
+	{
+		/** <A NAME="_ALLIED_MINOR_"></A>
+		 * Indicates that a units nationality is <B>Allied Minor</B>.
+		 */
 
-    public static final String AXIS_MINOR           = "Axis Minor";
+		ALLIED_MINOR("Allied Minor"),
 
-    /** <A NAME="_BRITISH_"></A>
-     * Indicates that a unit's nationality is <B>British</B>.
-     */
+		/** <A NAME="_AMERICAN_"></A>
+		 * Indicates that a units nationality is <B>American</B>.
+		 */
 
-    public static final String BRITISH              = "British";
+		AMERICAN("American"),
 
-    /** <A NAME="_FINNISH_"></A>
-     * Indicates that a unit's nationality is <B>Finnish</B>.
-     */
+		/** <A NAME="_AXIS_MINOR_"></A>
+		 * Indicates that a units nationality is <B>Axis Minor</B>.
+		 */
 
-    public static final String FINNISH              = "Finnish";
+		AXIS_MINOR("Axis Minor"),
 
-    /** <A NAME="_FRENCH_"></A>
-     * Indicates that a unit's nationality is <B>French</B>.
-     */
+		/** <A NAME="_BRITISH_"></A>
+		 * Indicates that a units nationality is <B>British</B>.
+		 */
 
-    public static final String FRENCH               = "French";
+		BRITISH("British"),
 
-    /** <A NAME="_GERMAN_"></A>
-     * Indicates that a unit's nationality is <B>German</B>.
-     */
+		/** <A NAME="_FINNISH_"></A>
+		 * Indicates that a units nationality is <B>Finnish</B>.
+		 */
 
-    public static final String GERMAN               = "German";
+		FINNISH("Finnish"),
 
-    /** <A NAME="_ITALIAN_"></A>
-     * Indicates that a unit's nationality is <B>Italian</B>.
-     */
+		/** <A NAME="_FRENCH_"></A>
+		 * Indicates that a units nationality is <B>French</B>.
+		 */
 
-    public static final String ITALIAN              = "Italian";
+		FRENCH("French"),
 
-    /** <A NAME="_JAPANESE_"></A>
-     * Indicates that a unit's nationality is <B>Japanese</B>.
-     */
+		/** <A NAME="_GERMAN_"></A>
+		 * Indicates that a units nationality is <B>German</B>.
+		 */
 
-    public static final String JAPANESE             = "Japanese";
+		GERMAN("German"),
 
-    /** <A NAME="_PARTISAN_"></A>
-     * Indicates that a unit's nationality is <B>Partisan</B>.
-     */
+		/** <A NAME="_ITALIAN_"></A>
+		 * Indicates that a units nationality is <B>Italian</B>.
+		 */
 
-    public static final String PARTISAN             = "Partisan";
+		ITALIAN("Italian"),
 
-    /** <A NAME="_RUSSIAN_"></A>
-     * Indicates that a unit's nationality is <B>Russian</B>.
-     */
+		/** <A NAME="_JAPANESE_"></A>
+		 * Indicates that a units nationality is <B>Japanese</B>.
+		 */
 
-    public static final String RUSSIAN              = "Russian";
+		JAPANESE("Japanese"),
 
-    /** <A NAME="_NATIONALITIES_"></A>
-     * A list of the recognized unit nationalities.
-     *
-     * @see #getNationality
-     */
+		/** <A NAME="_PARTISAN_"></A>
+		 * Indicates that a units nationality is <B>Partisan</B>.
+		 */
 
-    public static final String[] NATIONALITIES      = { ALLIED_MINOR, AMERICAN,
-                                                        AXIS_MINOR, BRITISH,
-                                                        FINNISH, FRENCH, GERMAN,
-                                                        ITALIAN, JAPANESE,
-                                                        PARTISAN, RUSSIAN };
+		PARTISAN("Partisan"),
 
-    /** <A NAME="_NATIONALITIES_VECTOR_"></A>
-     * An alternative method of accessing the list of recognized unit
-     * nationalities.
-     *
-     * @see #getNationality
-     */
+		/** <A NAME="_RUSSIAN_"></A>
+		 * Indicates that a units nationality is <B>Russian</B>.
+		 */
 
-    public static final Vector NATIONALITIES_VECTOR = new Vector(Arrays.asList(NATIONALITIES));
+		RUSSIAN("Russian");
 
-    /**
-     * The number of elements in the <A HREF="#_NATIONALITIES_">NATIONALITIES</A> array and
-     * <A HREF="#_NATIONALITIES_VECTOR_">NATIONALITIES_VECTOR</A>.
-     */
+		// Private data members
 
-    public static final int NATIONALITIES_LIST_SIZE = NATIONALITIES.length;
+		// The label associated with the enum constant.
 
-    /**
-     * Provides a label for a unit's nationality : <B>Nationality</B>
-     */
+		private final String label;
 
-    public static final String NATIONALITY_LABEL    = "Nationality";
+		// Constructor
 
-    // Access methods.
+		Nationalities(String label)
+		{
+			this.label = label;
+		}
 
-    /**
-     * Determine the nationality of a unit. The recognized values are listed
-     * below.
-     *
-     * @return a <CODE>String</CODE> specifying the unit's nationality.
-     *
-     * @see #ALLIED_MINOR
-     * @see #AMERICAN
-     * @see #AXIS_MINOR
-     * @see #BRITISH
-     * @see #FINNISH
-     * @see #FRENCH
-     * @see #GERMAN
-     * @see #ITALIAN
-     * @see #JAPANESE
-     * @see #PARTISAN
-     * @see #RUSSIAN
-     */
+		// Public access method
 
-    public abstract String getNationality();
+		/**
+		 * Returns the label associated with the enum constant.
+		 *
+		 * @return the <CODE>String</CODE> associated with the constant.
+		 */
+
+		public String label()
+		{
+			return label;
+		}
+	}
+
+	// Access methods
+
+	/**
+	 * Return the nationality of a unit.
+	 *
+	 * @return a <CODE>String</CODE> specifying the units nationality.
+	 */
+
+	public abstract String nationality();
 }
