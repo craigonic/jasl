1543452f1b910529009e4948831eb9543c9cacbe
Craig Campbell <craigonic@gmail.com>
2012-12-07 12:00:56 -0800

    Initial implementation of the Firepower interface.

---
+++ source/jasl/counters/Firepower.java
@@ -0,0 +1,94 @@
+// ************************************************************************** //
+// Firepower.java - This interface is part of the <B>counters</B> package, which     //
+//                  contains the class definitions and implementations for    //
+//                  objects used to represent the virtual playing pieces in   //
+//                  jASL.                                                     //
+//                                                                            //
+//                  NOTE: This program is based on Advanced Squad Leader,     //
+//                        which was created by The Avalon Hill Game Company,  //
+//                        Company, and lives on at <A HREF="http://www.multimanpublishing.com/Products/tabid/58/CategoryID/4/Default.aspx">Multi-Man Publishing</A>.      //
+//                                                                            //
+// Written By: Craig R. Campbell  -  December 2012                            //
+// ************************************************************************** //
+
+package jasl.counters;
+
+/**
+ * This interface is used to define the public constants and required methods
+ * associated with the firepower and range of a <A HREF="Fighting.html">Fighting</A> unit. The methods are
+ * intended for operation on the integer and String member variables within the
+ * implementing class.
+ *
+ * @version 1.0
+ * @author Copyright (C) 2012 Craig R. Campbell (craigonic@gmail.com)
+ * @see <A HREF="../../../source/jasl/counters/Firepower.html">Source code</A>
+ */
+
+public interface Firepower
+{
+	// Symbolic constants
+
+	// The following constants are provided primarily for use in displaying
+	// the values returned by the access methods in the output of a <A HREF="Unit.html">Unit</A>
+	// objects toString() method.
+
+	/**
+	 * Provides a label for a unit's firepower : <B>Firepower</B>
+	 */
+
+	public static final String FIREPOWER_LABEL = "Firepower";
+
+	/**
+	 * Provides a label for a unit's firepower equivalent:
+	 * <B>Firepower Equivalent</B>
+	 */
+
+	public static final String FIREPOWER_EQUIV_LABEL = "Firepower Equivalent";
+
+	/**
+	 * Provides a label for a unit's normal range : <B>Normal Range</B>
+	 */
+
+	public static final String NORMAL_RANGE_LABEL = "Normal Range";
+
+	// The following constants define the minimums for the attributes
+	// defined by this interface.
+
+	/** Minimum valid firepower equivalent value : <B>0</B> */
+
+	public static final int MIN_FIREPOWER = 0;
+
+	/** Minimum valid range value : <B>0</B> */
+
+	public static final int MIN_RANGE = 0;
+
+	// Access methods
+
+	/**
+	 * Return the firepower of a unit. This may be an infantry fire table
+	 * (IFT) value or a gun type designation (i.e. 88LL).
+	 *
+	 * @return a <CODE>String</CODE> specifying the unit's firepower.
+	 */
+
+	public abstract String firepower();
+
+	/**
+	 * Return the firepower equivalent of a unit. This is generally applied
+	 * to translate the capability of a gun designation (i.e. 88LL) into the
+	 * corresponding value on the infantry fire table (IFT).
+	 *
+	 * @return an <CODE>int</CODE> specifying the unit's firepower equivalent.
+	 */
+
+	public abstract int firepowerEquivalent();
+
+	/**
+	 * Return the maximum range that a unit may fire its weapon(s) at full
+	 * effect.
+	 *
+	 * @return an <CODE>int</CODE> specifying the normal range of the unit's weapon(s).
+	 */
+
+	public abstract int normalRange();
+}
