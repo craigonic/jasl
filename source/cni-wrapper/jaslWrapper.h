148e532903146d99f55cdfb9b143bad33a4bbe32
Craig Campbell <craigonic@gmail.com>
2014-02-13 13:07:54 -0800

    Additions to support transition of Driver.cpp to updated interfaces.

--- source/cni-wrapper/jaslWrapper.h
+++ source/cni-wrapper/jaslWrapper.h
@@ -5,19 +5,49 @@
  * of the public classes in the jasl libraries when accessed through the <A HREF="http://gcc.gnu.org/onlinedocs/gcj/About-CNI.html#About-CNI">CNI</A>
  * (Compiled Native Interface) wrapper.
  *
- * Written By  : Craig R. Campbell  -  June 2007
- *
- * $Id: jaslWrapper.h,v 1.2 2009/05/05 16:29:37 campbell Exp $
+ * Written By: Craig R. Campbell  -  June 2007
  */
 
-#ifndef JASL_WRAPPER_H // {
+#ifndef JASL_WRAPPER_H
 #define JASL_WRAPPER_H
 
-#include <jasl/counters/Unit.h>
-#include <jasl/counters/Leader.h>
-#include <jasl/counters/Squad.h>
+#include "jasl/counters/Classification$Classifications.h"
+#include "jasl/counters/Nationality$Nationalities.h"
+#include "jasl/counters/UnitType$InfantryTypes.h"
 
-#include <jasl/utilities/Dice.h>
+#include "jasl/counters/Unit.h"
+#include "jasl/counters/Leader.h"
+#include "jasl/counters/Squad.h"
+
+#include "jasl/ui/data/Game.h"
+#include "jasl/ui/data/Player.h"
+#include "jasl/ui/data/Side$Sides.h"
+
+#include "jasl/utilities/Dice.h"
+#include "jasl/utilities/Serialization.h"
+
+// Counters
+
+/**
+ * \typedef jasl::counters::Classification$Classifications Classifications
+ * \brief Shorthand declaration for the <A HREF="../../source/jasl/counters/Classification.html">Classifications</A> enum.
+ */
+
+typedef jasl::counters::Classification$Classifications Classifications;
+
+/**
+ * \typedef jasl::counters::Nationality$Nationalities Nationalities
+ * \brief Shorthand declaration for the <A HREF="../../source/jasl/counters/Nationality.html">Nationalities</A> enum.
+ */
+
+typedef jasl::counters::Nationality$Nationalities Nationalities;
+
+/**
+ * \typedef jasl::counters::UnitType$InfantryTypes InfantryTypes
+ * \brief Shorthand declaration for the <A HREF="../../source/jasl/counters/UnitType.html">InfantryTypes</A> enum.
+ */
+
+typedef jasl::counters::UnitType$InfantryTypes InfantryTypes;
 
 /**
  * \typedef jasl::counters::Unit Unit
@@ -40,6 +70,31 @@ typedef jasl::counters::Leader Leader;
 
 typedef jasl::counters::Squad Squad;
 
+// UI
+
+/**
+ * \typedef jasl::ui::data::Game Game
+ * \brief Shorthand declaration for a <A HREF="../../source/jasl/ui/data/Game.html">Game</A> object.
+ */
+
+typedef jasl::ui::data::Game Game;
+
+/**
+ * \typedef jasl::ui::data::Player Player
+ * \brief Shorthand declaration for a <A HREF="../../source/jasl/ui/data/Player.html">Player</A> object.
+ */
+
+typedef jasl::ui::data::Player Player;
+
+/**
+ * \typedef jasl::ui::data::Side$Sides Sides
+ * \brief Shorthand declaration for the <A HREF="../../source/jasl/ui/data/Side.html">Sides</A> enum.
+ */
+
+typedef jasl::ui::data::Side$Sides Sides;
+
+// Utilities
+
 /**
  * \typedef jasl::utilities::Dice Dice
  * \brief Shorthand declaration for a <A HREF="../../source/jasl/utilities/Dice.html">Dice</A> object.
@@ -47,4 +102,11 @@ typedef jasl::counters::Squad Squad;
 
 typedef jasl::utilities::Dice Dice;
 
-#endif // } JASL_WRAPPER_H
+/**
+ * \typedef jasl::utilities::Serialization Serialization
+ * \brief Shorthand declaration for the <A HREF="../../source/jasl/utilities/Serialization.html">Serialization</A> object.
+ */
+
+typedef jasl::utilities::Serialization Serialization;
+
+#endif // JASL_WRAPPER_H
