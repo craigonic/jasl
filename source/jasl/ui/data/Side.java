d1f67f8eed2c5f89f29b41c37f2c7810e10ab232
Craig Campbell <craigonic@gmail.com>
2016-02-23 12:52:37 -0800

    Added documentation to the Side class.
    The addPlayer() method was also updated to check the specified nationality
    against the ones available for the current side, returning false if it
    isn't.

--- source/jasl/ui/data/Side.java
+++ source/jasl/ui/data/Side.java
@@ -12,36 +12,40 @@
 
 package jasl.ui.data;
 
-import java.util.*; // For Map.
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import jasl.counters.Nationality;
 
 /**
- * This is a ...
+ * This class is used to manage the <A HREF="Player.html">player</A>s associated with a designated side
+ * in a <A HREF="Scenario.html">scenario</A>. It also includes an enum defining the available sides (Allies
+ * and Axis) and methods to provide a list of the nationalities on each of them.
  *
- * @version 1.4
- * @author Copyright (C) 2013-2015 Craig R. Campbell (craigonic@gmail.com)
+ * @version 1.5
+ * @author Copyright (C) 2013-2016 Craig R. Campbell (craigonic@gmail.com)
  * @see <A HREF="../../../../source/jasl/ui/data/Side.html">Source code</A>
  */
 
 public final class Side
 {
 	/**
-	 * Recognized side designations. These constants represent the counter
-	 * types that may be directly instantiated using the public classes in
-	 * the <B>counters</B> hierarchy.
+	 * Recognized side designations. These constants represent the side that
+	 * a combatant (player, counter, etc.) is associated with.
 	 */
 
 	public enum Sides
 	{
 		/**
-		 * Indicates that the ... <B>Allies</B>.
+		 * Indicates that the combatant is fighting for the <B>Allies</B>.
 		 */
 
 		ALLIES("Allies"),
 
 		/**
-		 * Indicates that the ... <B>Axis</B>.
+		 * Indicates that the combatant is fighting for one of the <B>Axis</B>
+		 * powers.
 		 */
 
 		AXIS("Axis");
@@ -73,9 +77,28 @@ public final class Side
 		}
 	}
 
+	// This variable is used to store the side that the players managed by
+	// an instance of this class, as well as the units they control/command,
+	// are fighting for.
+
 	private Sides _side;
 
+	// This variable is used to store a list of the players associated with
+	// the side that an instance of this class represents.
+
 	private Map<String,Player> _players;
+
+	// Constructor
+
+	/**
+	 * Construct a new <CODE>Side</CODE>.
+	 *
+	 * @param side the side that the instance is to represent
+	 * Example - <B>Sides.ALLIES</B>
+	 *
+	 * @see #player
+	 * @see #playerList
+	 */
 
 	public Side(Sides side)
 	{
@@ -84,9 +107,94 @@ public final class Side
 		_players = new LinkedHashMap<String,Player>();
 	}
 
+	// Public access methods
+
+	/**
+	 * Return a text representation of the attributes of a side.
+	 *
+	 * The output includes the name of the side, the nationalities
+	 * associated with it, and summary information for each player.
+	 *
+	 * @return a multi-line <CODE>String</CODE> specifying an overview of the elements of
+	 * the side.
+	 *
+	 * @see #unitList
+	 */
+
+	public String toText()
+	{
+		StringBuffer returnString = new StringBuffer();
+
+		returnString.append("Side:\t" + toString() + "\n\n");
+
+		returnString.append("\tNationalities:\t");
+
+		ArrayList<Nationality.Nationalities> nationalities =
+			(Side.Sides.ALLIES == _side) ?
+			alliedNationalities() : axisNationalities();
+
+		for (Nationality.Nationalities nationality : nationalities)
+		{
+			returnString.append("\"" + nationality.toString() + "\" ");
+		}
+
+		returnString.append("\n\n");
+
+		for (Map.Entry<String,Player> player : _players.entrySet())
+		{
+			returnString.append(player.getValue().toText());
+		}
+
+		// Return the completed string to calling program.
+
+		return returnString.toString();
+	}
+
+	/**
+	 * Return the name of a side.
+	 *
+	 * @return a <CODE>String</CODE> specifying the side's name.
+	 */
+
+	public String toString()
+	{
+		return _side.toString();
+	}
+
+	/**
+	 * Return the names of all the players on a side.
+	 *
+	 * @return an <CODE>ArrayList</CODE> of the names.
+	 */
+
+	public ArrayList<String> playerList()
+	{
+		return new ArrayList<String>(_players.keySet());
+	}
+
+	/**
+	 * Provide access to the data, including units, for a player.
+	 *
+	 * @return the <CODE>Player</CODE> instance associated with the specified name. The
+	 * return value is null if a player with the name does not exist on the
+	 * side.
+	 */
+
+	public Player player(String name)
+	{
+		return (!_players.isEmpty()) ? _players.get(name) : null;
+	}
+
+	/**
+	 * Return the nationalities associated with the Allies.
+	 *
+	 * @return an <CODE>ArrayList</CODE> of the corresponding nationalities.
+	 */
+
 	public static ArrayList<Nationality.Nationalities> alliedNationalities()
 	{
-		ArrayList<Nationality.Nationalities> nationalities = new ArrayList<Nationality.Nationalities>();
+		ArrayList<Nationality.Nationalities> nationalities =
+			new ArrayList<Nationality.Nationalities>();
 
 		nationalities.add(Nationality.Nationalities.AMERICAN);
 		nationalities.add(Nationality.Nationalities.BRITISH);
@@ -98,9 +206,16 @@ public final class Side
 		return nationalities;
 	}
 
+	/**
+	 * Return the nationalities associated with the Axis.
+	 *
+	 * @return an <CODE>ArrayList</CODE> of the corresponding nationalities.
+	 */
+
 	public static ArrayList<Nationality.Nationalities> axisNationalities()
 	{
-		ArrayList<Nationality.Nationalities> nationalities = new ArrayList<Nationality.Nationalities>();
+		ArrayList<Nationality.Nationalities> nationalities =
+			new ArrayList<Nationality.Nationalities>();
 
 		nationalities.add(Nationality.Nationalities.GERMAN);
 		nationalities.add(Nationality.Nationalities.JAPANESE);
@@ -110,6 +225,9 @@ public final class Side
 
 		return nationalities;
 	}
+
+	// This method is deprecated. It should be removed after the Scenario
+	// and Stack classes are fully implemented.
 
 	public static ArrayList<String> unitList(Nationality.Nationalities nationality)
 	{
@@ -217,11 +335,38 @@ public final class Side
 		return units;
 	}
 
+	// Public update methods
+
+	/**
+	 * Add a new player to a side.
+	 *
+	 * @param name the name of the new player
+	 * @param nationality the nationality of the majority of the units that
+	 * the player will command. Example - <B><A HREF="../../counters/Nationality.html#_FRENCH_">FRENCH</A></B>
+	 * @param entryTurn the turn when the units initially managed by the
+	 * player enter the game.
+	 * Example - <B>3</B>
+	 *
+	 * @return a <CODE>boolean</CODE> indicating if the player was added successfully or
+	 * not. The value may be false if the name matches that of an existing
+	 * player or if the nationality does not belong to the side. The
+	 * generation of the new <A HREF="Player.html">Player</A> instance will also throw an exception if
+	 * the name or entryTurn argument is invalid.
+	 */
+
 	public boolean addPlayer(String name,
 	                         Nationality.Nationalities nationality,
 	                         int entryTurn)
 	{
-		if (!_players.containsKey(name))
+		// Verify that the specified nationality matches one of the ones
+		// associated with the side this instance represents.
+
+		ArrayList<Nationality.Nationalities> validNationalities =
+			(Side.Sides.ALLIES == _side) ? alliedNationalities() :
+			                               axisNationalities();
+
+		if (validNationalities.contains(nationality) &&
+		    (!_players.containsKey(name)))
 		{
 			_players.put(name,
 			             new Player(name,nationality,entryTurn));
@@ -230,49 +375,5 @@ public final class Side
 		}
 
 		return false;
-	}
-
-	public ArrayList<String> playerList()
-	{
-		return new ArrayList<String>(_players.keySet());
-	}
-
-	public Player player(String name)
-	{
-		return (!_players.isEmpty()) ? _players.get(name) : null;
-	}
-
-	public String toString()
-	{
-		return _side.toString();
-	}
-
-	public String toText()
-	{
-		StringBuffer returnString = new StringBuffer();
-
-		returnString.append("Side:\t" + toString() + "\n\n");
-
-		returnString.append("\tNationalities:\t");
-
-		ArrayList<Nationality.Nationalities> nationalities =
-			(Side.Sides.ALLIES == _side) ?
-			alliedNationalities() : axisNationalities();
-
-		for (Nationality.Nationalities nationality : nationalities)
-		{
-			returnString.append("\"" + nationality.toString() + "\" ");
-		}
-
-		returnString.append("\n\n");
-
-		for (Map.Entry<String,Player> player : _players.entrySet())
-		{
-			returnString.append(player.getValue().toText());
-		}
-
-		// Return the completed string to calling program.
-
-		return returnString.toString();
 	}
 }
