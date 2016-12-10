2e6966cbc161abb33fb4fe9330cef54c841c9589
Craig Campbell <craigonic@gmail.com>
2016-12-09 22:27:27 -0800

    Added test cases for the updated Player and Stack classes.

--- source/Driver.java
+++ source/Driver.java
@@ -9,6 +9,9 @@
 // Written By: Craig R. Campbell  -  December 1998                            //
 // ************************************************************************** //
 
+import java.util.Map;
+import java.util.List;
+
 import jasl.counters.*;
 import jasl.ui.data.*;
 import jasl.utilities.Dice;
@@ -526,6 +529,337 @@ public class Driver
             }
         }
 
+        // Test the Player and Stack classes.
+
+        System.out.println("Testing Exception handling during Player creation:");
+
+        Player playerObject = null;
+
+        // Null Name
+
+        System.out.println("\nNull name parameter:\n");
+
+        try
+        {
+            playerObject = new Player(null,
+                                      Nationality.Nationalities.PARTISAN,1);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Blank Name
+
+        System.out.println("\nZero-length name parameter:\n");
+
+        try
+        {
+            playerObject = new Player("",Nationality.Nationalities.PARTISAN,1);
+        }
+
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Invalid Entry Turn (less than 1)
+
+        System.out.println("\nInvalid (less than 1) entry turn:\n");
+
+        try
+        {
+            playerObject = new Player("Dr. Pepper",
+                                      Nationality.Nationalities.PARTISAN,-1);
+        }
+
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        System.out.println("\nTesting Exception handling for Player methods:");
+
+        // Null Unit parameter to addUnit()
+
+        playerObject = new Player("Dr. Pepper",
+                                  Nationality.Nationalities.PARTISAN,1);
+
+        System.out.println("\nNull Unit parameter to addUnit():\n");
+
+        try
+        {
+            playerObject.addUnit(null);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Null Stack parameter to addStack()
+
+        System.out.println("\nNull Stack parameter to addStack():\n");
+
+        try
+        {
+            playerObject.addStack(null);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Confirm that the Player's Unit list is empty.
+
+        assert(playerObject.stackList().isEmpty());
+
+        // Add some valid Units to the Player object.
+
+        System.out.println("\nAssign some Units to the Player:");
+
+        playerObject.addUnit("7-0 Leader (Dr. Pepper)");
+        playerObject.addUnit("3-3-7 Squad (A)");
+        playerObject.addUnit("3-3-7 Squad (B)");
+        playerObject.addUnit("3-3-7 Squad (C)");
+        playerObject.addUnit("2-6 LMG (X)");
+
+        // Display all of the entered values for this instance using the
+        // toText() method.
+
+        System.out.println("\nPlayer.toText() output:\n\n" +
+                           playerObject.toText());
+
+        // Retrieve a list of the Units (in Stacks) assigned to the Player.
+
+        List<Stack> unitList = playerObject.stackList();
+
+        // Invalid index parameter to takeStack()
+
+        Stack temporaryStack;
+
+        System.out.println("Invalid (Stack) list index:\n");
+
+        try
+        {
+            temporaryStack = playerObject.takeStack(unitList.size());
+        }
+
+        catch (IndexOutOfBoundsException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Create Stacks for use in testing invalid argument handling in
+        // Stack.addPortagedItem() and Stack.addSubStack().
+
+        System.out.println("\nTesting Exception handling during Stack creation:");
+
+        String testUnit     = null;
+        Stack unitTestStack = null;
+
+        // Null Unit
+
+        System.out.println("\nNull Unit parameter:\n");
+
+        try
+        {
+            unitTestStack = new Stack(testUnit);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Null Stack
+
+        System.out.println("\nNull Stack parameter:\n");
+
+        try
+        {
+            unitTestStack = new Stack(unitTestStack);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        testUnit = "6+1 Leader (Sgt. Stedenko)";
+
+        unitTestStack = new Stack(testUnit,7,77);
+
+        Stack subStackTestStack = new Stack(unitTestStack);
+
+        // Confirm that a Stack managing a Unit doesn't have sub-stacks.
+
+        assert(null == unitTestStack.subStacks());
+
+        // Confirm that a Stack managing a group of sub-stacks doesn't have
+        // any portaged items.
+
+        assert(null == subStackTestStack.portagedItems());
+
+        // Invalid Stack
+
+        System.out.println("\nInvalid Stack parameter:\n");
+
+        try
+        {
+            subStackTestStack = new Stack(subStackTestStack);
+        }
+
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        System.out.println("\nTesting Exception handling for Stack methods:");
+
+        // Null Stack parameter to addPortagedItem()
+
+        System.out.println("\nNull Stack parameter to addPortagedItem():\n");
+
+        try
+        {
+            unitList.get(2).addPortagedItem(null);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Invalid Stack parameter to addPortagedItem()
+
+        System.out.println("\nInvalid Stack parameter to addPortagedItem():\n");
+
+        try
+        {
+            unitList.get(2).addPortagedItem(subStackTestStack);
+        }
+
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        System.out.println("\nTesting the operations of the Player and Stack classes:");
+
+        // Have the second Squad in the list carry (portage) the machine gun.
+
+        System.out.println("\nThe second Squad will carry the machine gun:");
+
+        temporaryStack = playerObject.takeStack(unitList.size() - 1);
+
+        unitList.get(2).addPortagedItem(temporaryStack);
+
+        System.out.println("\nPlayer.toText() output:\n\n" +
+                           playerObject.toText());
+
+        // Pass the machine gun from the second squad to the first.
+
+        System.out.println("Now it's the first Squad's turn to carry it:");
+
+        unitList = playerObject.stackList();
+
+        Map<Integer,Stack> portagedItems = unitList.get(2).portagedItems();
+
+        temporaryStack =
+            unitList.get(2).takePortagedItem(portagedItems.keySet().iterator().next());
+
+        unitList.get(1).addPortagedItem(temporaryStack);
+
+        System.out.println("\nPlayer.toText() output:\n\n" +
+                           playerObject.toText());
+
+        // Null Stack parameter to addSubStack()
+
+        System.out.println("Null Stack parameter to Stack.addSubStack():\n");
+
+        try
+        {
+            unitList.get(2).addSubStack(null);
+        }
+
+        catch (NullPointerException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Invalid Stack parameter to addSubStack()
+
+        System.out.println("\nInvalid Stack parameter to Stack.addSubStack():\n");
+
+        try
+        {
+            unitList.get(2).addSubStack(subStackTestStack);
+        }
+
+        catch (IllegalArgumentException e)
+        {
+            System.out.println("Caught: " + e);
+        }
+
+        // Group all of the Units into a single Stack.
+
+        System.out.println("\nAll of the Units in a single stack:");
+
+        Stack newStack = new Stack(playerObject.takeStack(0));
+
+        int i = 0;
+        int stackListSize = playerObject.stackList().size();
+
+        while (i++ < stackListSize)
+        {
+            newStack.addSubStack(playerObject.takeStack(0));
+        }
+
+        playerObject.addStack(newStack);
+
+        System.out.println("\nPlayer.toText() output:\n\n" +
+                           playerObject.toText());
+
+        // Set the position of the (single / combined) Stack.
+
+        System.out.println("Set the position of the stack to 1H5:");
+
+        unitList = playerObject.stackList();
+
+        unitList.get(0).setPositionLabel("1H5");
+
+        System.out.println("\nPlayer.toText() output:\n\n" +
+                           playerObject.toText());
+
+        // Move the first Squad in the (single / combined) Stack to a separate
+        // position.
+
+        System.out.println("Move the first Squad to a different position (1F7):");
+
+        temporaryStack = playerObject.stackList().get(0);
+
+        Map<Integer,Stack> subStacks = temporaryStack.subStacks();
+
+        System.out.println("\nThe individual Stacks within the combined one are:\n");
+
+        for (Map.Entry<Integer,Stack> entry : subStacks.entrySet())
+        {
+            System.out.println("\tKey: " + entry.getKey().toString() +
+                               "\t"      + entry.getValue().toText());
+        }
+
+        newStack = temporaryStack.takeSubStack(Integer.valueOf(1));
+
+        newStack.setPositionLabel("1F7");
+
+        playerObject.addStack(newStack);
+
+        System.out.println("\nPlayer.toText() output:\n\n" +
+                           playerObject.toText());
+
         // Test the Game class.
 
         System.out.println("Testing the operations of the Game class:\n");
@@ -548,6 +882,8 @@ public class Driver
         axisPlayer.addUnit("6-5-8 Squad");
         axisPlayer.addUnit("6-5-8 Squad");
         axisPlayer.addUnit("6-5-8 Squad");
+        axisPlayer.addUnit("3-8 LMG");
+        axisPlayer.addUnit("3-8 LMG");
 
         System.out.print(Game.game().toText());
         System.out.println();
