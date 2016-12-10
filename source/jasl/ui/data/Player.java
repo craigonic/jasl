d0ce0a0074192c35e48d8f1dc03e2a5fb459ccb4
Craig Campbell <craigonic@gmail.com>
2016-12-09 22:19:45 -0800

    Reworked to manage Units as Stacks rather than directly.

--- source/jasl/ui/data/Player.java
+++ source/jasl/ui/data/Player.java
@@ -24,9 +24,65 @@ import jasl.utilities.Messages;
  * This class is used to provide data management for a player during a jASL
  * session. It includes the player's name, the nationality of the majority of
  * the <A HREF="../../counters/Unit.html">Unit</A>s under his or her control, and the turn that he or she enters the
- * game, as well as the ability to add, delete, and access individual Units.
+ * game.
+ * <P>
+ * <B>Unit Management</B>
+ * <P>
+ * This section describes the recommended procedures for adding, accessing, and
+ * grouping units.
+ * <P>
+ * Before the actual battle begins, Unit objects should be assigned to a player
+ * using the addUnit() method. This "wraps" each item in a <A HREF="Stack.html">Stack</A> object,
+ * allowing them to be managed as groups (e.g. portaged items, a set of items
+ * that act (move, fire, etc.) together). The "stacks" managed by a player are
+ * accessible through the stackList() method. Its primary purpose is to provide
+ * the index of an item to be manipulated. This index is used as an argument to
+ * the takeStack() method. Individual stacks must first be "taken" (i.e.
+ * referenced by a temporary variable) before being moved. Note that (for now)
+ * only one stack may be "taken" at a time.
+ * <P>
+ * <I>Example 1: Portaging an item</I>
+ * <P>
+ * - Retrieve a list of Units (Stacks) using the stackList() method. <P>
+ * - "Take" the Unit to be portaged, by specifying its index to takeStack(). <P>
+ * - Referencing the desired owner Unit (Stack) by its index (use the get()
+ *   method of List), call the Stack.addPortagedItem() method, passing the
+ *   "taken" (i.e. to be portaged) object as the argument.
+ * <P>
+ * <I>Example 2: Transferring a portaged item from one Unit to another</I>
+ * <P>
+ * - Retrieve a list of Units (Stacks) using the stackList() method. <P>
+ * - Select the Unit (Stack) that is currently carrying the item and use it to
+ *   call Stack.portagedItems(). This method will return a Map. <P>
+ * - Using the selected Unit object, call Stack.takePortagedItem(), passing the
+ *   "key" associated with the desired (portaged) item. Keep a reference to the
+ *   returned object. <P>
+ * - From the list returned by stackList(), select the Unit (Stack) that the
+ *   item should be transferred to. Use it to call Stack.addPortagedItem(),
+ *   passing the referenced object as an argument.
+ * <P>
+ * <I>Example 3: Grouping Units into a single stack</I>
+ * <P>
+ * - Create a new Stack object, passing the first stack to be included
+ *   (retrieved as above using the takeStack() method) in the new group as an
+ *   argument. <P>
+ * - Retrieve additional stacks as desired (again using takeStack()) and add
+ *   them to the new Stack object using Stack.addSubStack(). Note that the
+ *   indexes of the source stack (retrieved via stackList()) change after each
+ *   takeStack() call. <P>
+ * - Pass the new Stack object as an argument to addStack().
+ * <P>
+ * <I>Example 4: Removing a single stack (Unit and portaged item(s)) from a group of stacks</I>
+ * <P>
+ * - Using the stackList() method and the get() method of List, reference the
+ *   desired Stack object (group of stacks). <P>
+ * - Call Stack.subStacks() using the referenced object. This method returns
+ *   a Map, which provides a "key" for each sub-stack. <P>
+ * - Using the initial (group) Stack object, pass the desired "key" as an
+ *   argument to Stack.takeSubStack(). <P>
+ * - Pass the item returned by takeSubStack() to the addStack() method.
  *
- * @version 1.2
+ * @version 2.0
  * @author Copyright (C) 2013-2016 Craig R. Campbell (craigonic@gmail.com)
  * @see <A HREF="../../../../source/jasl/ui/data/Player.html">Source code</A>
  */
@@ -41,7 +97,7 @@ public final class Player
 	private String _name;
 
 	// This variable contains the nationality (e.g. "American" or "German")
-	// of the majority of the <A HREF="../../counters/Unit.html">Unit</A>s that the player controls. It is intended
+	// of the majority of the Units that the player controls. It is intended
 	// primarily for informational use, since a player could potentially
 	// control items (counters) with no nationality or a nationality
 	// associated with the other "side" (e.g. captured items).
@@ -54,17 +110,17 @@ public final class Player
 
 	private int _entryTurn;
 
-	// This variable is used to store the units managed by the player. The
-	// list is updated using the addUnit() and deleteUnit() methods. The
-	// labels associated with the units are available through unitList() and
-	// unit() may be used to access individual Unit objects.
+	// This variable is used to store the units (in Stacks) managed by the
+	// player. The list is updated using the addUnit(), addStack(), and
+	// takeStack() methods. Units (via a Stack) can be accessed through
+	// stackList().
 
-	private ArrayList<String> _unitList;
+	private ArrayList<Stack> _stackList;
 
 	// The following strings are used as messages for any exceptions that
 	// may be generated by bad data being passed to the constructor.
 
-	private static final String CLASS_NAME = "Player";
+	private static final String CLASS_NAME = Player.class.getSimpleName();
 
 	private static final String nullPointerError =
 		Messages.buildErrorMessage(CLASS_NAME,Messages.CONSTRUCTOR,
@@ -82,12 +138,11 @@ public final class Player
 	/**
 	 * Construct a new <CODE>Player</CODE>.
 	 *
-	 * @param name the name of the player
+	 * @param name the name of the player.
 	 * @param nationality the nationality of the majority of the units
 	 * that the player controls. Example - <B><A HREF="../../counters/Nationality.html#_GERMAN_">GERMAN</A></B>
 	 * @param entryTurn the turn when the units initially managed by the
-	 * player enter the game.
-	 * Example - <B>1</B>
+	 * player enter the game. Example - <B>1</B>
 	 *
 	 * @throws NullPointerException in the case of a null name argument.
 	 * @throws IllegalArgumentException in the case of an empty name or an
@@ -107,7 +162,7 @@ public final class Player
 			throw new NullPointerException(nullPointerError);
 		}
 
-		if (0 == name.length())
+		if (name.isEmpty())
 		{
 			throw new IllegalArgumentException(zeroLengthArgumentError);
 		}
@@ -128,21 +183,23 @@ public final class Player
 
 		_entryTurn = entryTurn;
 
-		_unitList = new ArrayList<String>();
+		// Empty list of Stacks.
+
+		_stackList = new ArrayList<Stack>();
 	}
 
 	// Public access methods
 
 	/**
 	 * Return a text representation of the attributes of a player.
-	 *
-	 * The output includes the labels associated with the units managed by
-	 * the player.
+	 * <P>
+	 * The output includes basic information about the units managed by the
+	 * player.
 	 *
 	 * @return a multi-line <CODE>String</CODE> specifying the current settings of an
 	 * instance of this type.
 	 *
-	 * @see #unitList
+	 * @see #stackList
 	 */
 
 	public String toText()
@@ -156,9 +213,9 @@ public final class Player
 
 		returnString.append("\n\tUnits:\n");
 
-		for (String unit : unitList())
+		for (Stack stack : stackList())
 		{
-			returnString.append("\t\t" + unit + "\n");
+			returnString.append("\t\t" + stack.toText() + "\n");
 		}
 
 		// Return the completed string to calling program.
@@ -202,57 +259,31 @@ public final class Player
 	}
 
 	/**
-	 * Return one of the units managed by a player.
+	 * Provide access to the units (in their respective Stacks) managed by a
+	 * player.
 	 *
-	 * @param index the (zero-based) index of the <A HREF="../../counters/Unit.html">Unit</A> to be accessed
-	 *
-	 * @return the <CODE>Unit</CODE> at the specified index.
-	 *
-	 * @throws IndexOutOfBoundsException in the case where index is less than
-	 * zero or greater than the number of units in the list.
-	 *
-	 * @see #unitList
+	 * @return an <B>unmodifiable</B> <CODE>List</CODE> of the player's Stack items.
 	 */
 
-	public String unit(int index)
+	public List<Stack> stackList()
 	{
-		String unitToBeReturned;
-
-		try
-		{
-			unitToBeReturned = _unitList.get(index);
-		}
-
-		catch (IndexOutOfBoundsException exception)
-		{
-			throw new IndexOutOfBoundsException(Messages.buildErrorMessage(CLASS_NAME,
-			                                                               "unit",
-			                                                               Messages.INVALID_PARAMETER_MSG + index));
-		}
-
-		return unitToBeReturned;
-	}
-
-	/**
-	 * Return a list of descriptions of the units managed by a player.
-	 *
-	 * @return a <CODE>List</CODE> of String items specifying the labels associated with
-	 * the player's units.
-	 */
-
-	public List<String> unitList()
-	{
-		return Collections.unmodifiableList(_unitList);
+		return Collections.unmodifiableList(_stackList);
 	}
 
 	// Public update methods
 
 	/**
-	 * Place a new unit under a player's command.
+	 * Place a unit under a player's command.
+	 * <P>
+	 * The Unit will be wrapped/managed by a Stack object. Note that this
+	 * method is intended for use only in initial Unit assignment (i.e. it
+	 * should not be called after active play has begun).
 	 *
-	 * @param unit the <A HREF="../../counters/Unit.html">Unit</A> to be added
+	 * @param unit the Unit to be added.
 	 *
-	 * @throws NullPointerException in the case of a null object.
+	 * @throws NullPointerException in the case of a null Unit object.
+	 *
+	 * @see #addStack
 	 */
 
 	public void addUnit(String unit)
@@ -264,44 +295,60 @@ public final class Player
 			                                                          Messages.NULL_PARAMETER_MSG));
 		}
 
-		_unitList.add(unit);
+		_stackList.add(new Stack(unit));
 	}
 
 	/**
-	 * Remove a unit under a player's command.
+	 * Add another Stack to the group managed by a player.
 	 *
-	 * If the unit to be removed is currently managing (i.e. portaging)
-	 * other Units (e.g. support weapons), those items will be placed back
-	 * into the general list after the specified unit is deleted.
+	 * @param stack the Stack to be added.
 	 *
-	 * @param index the (zero-based) index of the <A HREF="../../counters/Unit.html">Unit</A> to be removed
-	 *
-	 * @throws IndexOutOfBoundsException in the case where index is less than
-	 * zero or greater than the number of units in the list.
+	 * @throws NullPointerException in the case of a null Stack object.
 	 */
 
-	public void deleteUnit(int index)
+	public void addStack(Stack stack)
 	{
-		String unitToBeDeleted;
+		if (null == stack)
+		{
+			throw new NullPointerException(Messages.buildErrorMessage(CLASS_NAME,
+			                                                          "addStack",
+			                                                          Messages.NULL_PARAMETER_MSG));
+		}
+
+		_stackList.add(stack);
+	}
+
+	/**
+	 * Return the sub-stack referenced by the specified index.
+	 * <P>
+	 * If the Stack to be removed is currently managing (i.e. portaging)
+	 * other Units (e.g. support weapons) and/or contains a group of Stacks,
+	 * all of those items will be included with the returned object.
+	 *
+	 * @param index the (zero-based) index of the Stack to be removed.
+	 *
+	 * @return the <CODE>Stack</CODE> at the index matching the argument.
+	 *
+	 * @throws IndexOutOfBoundsException in the case where index is less than
+	 * zero or greater than the number of Stacks in the list.
+	 */
+
+	public Stack takeStack(int index)
+	{
+		Stack departingStack;
 
 		try
 		{
-			unitToBeDeleted = _unitList.get(index);
+			departingStack = _stackList.remove(index);
 		}
 
 		catch (IndexOutOfBoundsException exception)
 		{
 			throw new IndexOutOfBoundsException(Messages.buildErrorMessage(CLASS_NAME,
-			                                                               "deleteUnit",
+			                                                               "takeStack",
 			                                                               Messages.INVALID_PARAMETER_MSG + index));
 		}
 
-		// Place any Unit objects that are be managed (e.g. support
-		// weapons that are being portaged) by the unit to be removed in
-		// a separate, temporary list. Once the specified unit has been
-		// deleted, add the items formerly under its control to the main
-		// list.
-
-		_unitList.remove(index);
+		return departingStack;
 	}
 }
