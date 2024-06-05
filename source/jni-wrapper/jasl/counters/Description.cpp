afcdc4bcb02513e9ff84f6d24b7ed8b481e2193a
Craig Campbell <craigonic@gmail.com>
2024-06-04 21:06:16 -0700

    Initial implementation of the proxy interface & enum for Description(s).

---
+++ source/jni-wrapper/jasl/counters/Description.cpp
@@ -0,0 +1,35 @@
+/**
+ * \file Description.cpp
+ *
+ * This file defines a proxy intended to mirror the elements found in the
+ * <A HREF="../../../jasl/counters/Description.html">Descriptions</A> Enum, which is implemented in Java, for use in C++ programs.
+ *
+ * Written By: Craig R. Campbell  -  March 2024
+ */
+
+#include "Description.h"
+
+#include "JaslEnums.h"
+
+static struct JaslEnumInterface::JaslEnumData descriptionsEnumData =
+	{"jasl/counters/Description$Descriptions",nullptr,nullptr,nullptr,nullptr,nullptr};
+
+// The returned string for the following method is a copy of a Java String,
+// converted to the indicated type using the <A HREF="../../JniWrapper.h.html#_JS2SS_">js2ss</A>() function.
+
+// toString: Return the label associated with the specifed enum value.
+
+std::string toString(Descriptions description) noexcept
+{
+	return JaslEnumInterface::convertToString(descriptionsEnumData,
+	                                          static_cast<int>(description));
+}
+
+// toObject: Return an instance of a (Java) Description.Descriptions object,
+//           based on the specified value.
+
+jobject toObject(Descriptions description) noexcept
+{
+	return JaslEnumInterface::convertToObject(descriptionsEnumData,
+	                                          static_cast<int>(description));
+}
---
+++ source/jni-wrapper/jasl/counters/Description.h
@@ -0,0 +1,49 @@
+/**
+ * \file Description.h
+ *
+ * This file declares a proxy intended to mirror the elements found in the
+ * <A HREF="../../../jasl/counters/Description.html">Descriptions</A> Enum, which is implemented in Java, for use in C++ programs.
+ *
+ * Written By: Craig R. Campbell  -  March 2024
+ */
+
+#pragma once
+
+#include <jni.h>
+
+#include <string>
+
+/**
+ * \brief <A HREF="../../../jasl/counters/Description.html">Descriptions</A> enum JNI (Java Native Interface) proxy.
+ *
+ * This enum provides a subset of the functionality of its namesake, which is
+ * implemented in Java.
+ *
+ * @version 0.1
+ * @author Copyright (C) 2024 Craig R. Campbell (craigonic@gmail.com)
+ * @see <A HREF="../../../source/jni-wrapper/jasl/counters/Description.h.html">Source code</A>
+ */
+
+enum class Descriptions : int
+{
+	Crew,
+	HalfSquad,
+	Leader,
+	Squad
+};
+
+// The returned string for the following method is a copy of a Java String,
+// converted to the indicated type using the <A HREF="../../JniWrapper.h.html#_JS2SS_">js2ss</A>() function.
+
+/**
+ * \brief Return the label associated with the specifed enum value.
+ */
+
+std::string toString(Descriptions description) noexcept;
+
+/**
+ * \brief Return an instance of a (Java) Description.Descriptions object, based
+ * on the specified value.
+ */
+
+jobject toObject(Descriptions description) noexcept;
